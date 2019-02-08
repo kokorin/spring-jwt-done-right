38b40610caf200ad51cb4eaed3b4159e5139e694
Denis Kokorin <kokorin86@gmal.com>
2019-02-08 07:14:51 +0200

    Initial commit

---
+++ src/main/java/com/github/kokorin/springjwtdoneright/SpringJwtDoneRightApplication.java
@@ -0,0 +1,14 @@
+package com.github.kokorin.springjwtdoneright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtDoneRightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtDoneRightApplication.class, args);
+	}
+
+}
+
